d7055d025ad3c9f85b4cfebe9f6f9b7e47f1989f
p3xny <x17project@gmail.com>
2025-08-04 15:41:22 +0200

    Changed the port and host url for backend

--- Backend/app.py
+++ Backend/app.py
@@ -92,4 +92,4 @@ async def send_message(payload: RequestPayload):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
