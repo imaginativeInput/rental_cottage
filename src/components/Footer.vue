aefb613a3060e64d89f35ace46e1a80e03c87e92
imaginativeInput <imaginative.input@gmail.com>
2026-05-28 22:35:55 +0200

    Revert "Use rezerwacja@domekrzepiska.pl in the footer (both mailto and visible text)."
    This reverts commit 656ff84616dc13bf5a3ad2b2219eb00b1e570f0d.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,13 +11,13 @@
           <span class="phone-number">+48 692 434 000</span>
         </a>
 
-        <a class="email-link" href="mailto:rezerwacja@domekrzepiska.pl">
+        <a class="email-link" href="mailto:rzepiskadomek@gmail.com">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="email-icon">
             <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
             <path
               d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
           </svg>
-          <span class="email-text">rezerwacja@domekrzepiska.pl</span>
+          <span class="email-text">rzepiskadomek@gmail.com</span>
         </a>
       </div>
 
